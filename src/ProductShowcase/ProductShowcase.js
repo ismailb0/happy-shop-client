e1b2b7902c0abab30060c41d3a09a76ad3c34470
Ismail Benmoussa <ismailbenmoussa@gmail.com>
2019-03-09 19:18:48 +0100

    Make product showcase dynamic

--- src/ProductShowcase/ProductShowcase.js
+++ src/ProductShowcase/ProductShowcase.js
@@ -3,81 +3,54 @@ import React, { Component } from 'react';
 import ProductCard from '../ProductCard';
 import './ProductShowcase.css';
 import reptile from './static/contemplative-reptile.jpg';
+import { PRODUCTS } from '../constants.js'
 
 class ProductShowcase extends Component {
+
+  getProductRows = () => {
+    const productRows = [];
+    let startIndex = 0;
+    let endIndex = 3;
+
+    while (endIndex < PRODUCTS.length + 3) {
+      const endSlice = (PRODUCTS.length - startIndex) < 3 ?  PRODUCTS.length : endIndex;
+      // productRow.push(PRODUCTS.slice(startIndex, endSlice))
+      const productRow = Array.prototype.slice.call(PRODUCTS, startIndex, endSlice)
+      productRows.push(productRow)
+      startIndex += 3;
+      endIndex += 3;
+    }
+    return Array.from(productRows);
+  }
+
+  getProductCards = () => {
+    const productRows = this.getProductRows()
+    return productRows.map((productRow) => {
+      return (
+        <div className='product-showcase-row'>
+          {
+            productRow.map((product) => {
+              return (
+                <ProductCard
+                  key={product.productName}
+                  image={reptile}
+                  title={product.title}
+                  productBrand={product.productBrand}
+                  productName={product.productName}
+                  productPrice={product.productPrice}
+                />
+              );
+            })
+          }
+        </div>
+      );
+    })
+  }
 
   render() {
     return (
       <div className='product-showcase-wrapper'>
-        <div className='product-showcase-row'>
-          <ProductCard
-            image={reptile}
-            title="Contemplative Reptile"
-            productBrand="KAT VON D"
-            productName="Lock-it Foundation"
-            productPrice={56.00}
-          />
-          <ProductCard
-            image={reptile}
-            title="Contemplative Reptile"
-            productBrand="KAT VON D"
-            productName="Lock-it Foundation"
-            productPrice={56.00}
-          />
-          <ProductCard
-            image={reptile}
-            title="Contemplative Reptile"
-            productBrand="KAT VON D"
-            productName="Lock-it Foundation"
-            productPrice={56.00}
-          />
-        </div>
-        <div className='product-showcase-row'>
-          <ProductCard
-            image={reptile}
-            title="Contemplative Reptile"
-            productBrand="KAT VON D"
-            productName="Lock-it Foundation"
-            productPrice={56.00}
-          />
-          <ProductCard
-            image={reptile}
-            title="Contemplative Reptile"
-            productBrand="KAT VON D"
-            productName="Lock-it Foundation"
-            productPrice={56.00}
-          />
-          <ProductCard
-            image={reptile}
-            title="Contemplative Reptile"
-            productBrand="KAT VON D"
-            productName="Lock-it Foundation"
-            productPrice={56.00}
-          />
-        </div>
-        <div className='product-showcase-row'>
-          <ProductCard
-            image={reptile}
-            title="Contemplative Reptile"
-            productBrand="KAT VON D"
-            productName="Lock-it Foundation"
-            productPrice={56.00}
-          />
-          <ProductCard
-            image={reptile}
-            title="Contemplative Reptile"
-            productBrand="KAT VON D"
-            productName="Lock-it Foundation"
-            productPrice={56.00}
-          />
-          <ProductCard
-            image={reptile}
-            title="Contemplative Reptile"
-            productBrand="KAT VON D"
-            productName="Lock-it Foundation"
-            productPrice={56.00}
-          />
-        </div>
+        {this.getProductCards()}
       </div>
     );
   }
---
+++ src/constants.js
@@ -0,0 +1,68 @@
+export const PRODUCTS = [
+  {
+    title: "Contemplative Reptile",
+    productBrand: "KAT VON D",
+    productName: "Lock-it Foundation",
+    productPrice: 56.00
+  },
+  {
+    title: "Contemplative Reptile",
+    productBrand: "MAKE UP FOR EVER",
+    productName: "Ultra HD Foundation",
+    productPrice: 72.00
+  },
+  {
+    title: "Contemplative Reptile",
+    productBrand: "COVER FX",
+    productName: "Custom Cover Drops",
+    productPrice: 68.00
+  },
+  {
+    title: "Contemplative Reptile",
+    productBrand: "KAT VON D",
+    productName: "Lock-it Foundation",
+    productPrice: 56.00
+  },
+  {
+    title: "Contemplative Reptile",
+    productBrand: "MAKE UP FOR EVER",
+    productName: "Ultra HD Foundation",
+    productPrice: 72.00
+  },
+  {
+    title: "Contemplative Reptile",
+    productBrand: "COVER FX",
+    productName: "Custom Cover Drops",
+    productPrice: 68.00
+  },
+  {
+    title: "Contemplative Reptile",
+    productBrand: "KAT VON D",
+    productName: "Lock-it Foundation",
+    productPrice: 56.00
+  },
+  {
+    title: "Contemplative Reptile",
+    productBrand: "KAT VON D",
+    productName: "Lock-it Foundation",
+    productPrice: 56.00
+  },
+  {
+    title: "Contemplative Reptile",
+    productBrand: "MAKE UP FOR EVER",
+    productName: "Ultra HD Foundation",
+    productPrice: 72.00
+  },
+  {
+    title: "Contemplative Reptile",
+    productBrand: "COVER FX",
+    productName: "Custom Cover Drops",
+    productPrice: 68.00
+  },
+  {
+    title: "Contemplative Reptile",
+    productBrand: "MAKE UP FOR EVER",
+    productName: "Ultra HD Foundation",
+    productPrice: 72.00
+  },
+];
